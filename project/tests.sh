01802238520f214fa70ca32046df3f8054456ba2
Muhammad Naeem Ur Rehman <maliknaeemurrehman81@gmail.com>
2023-11-25 15:31:32 +0100

    update test.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pytest /home/runner/work/made-project-FAU/project/pipeLine.py  
+pytest /home/runner/work/made-project-FAU/main/project/pipeLine.py  
 
 # Execute the data pipeline
-python3 /home/runner/work/made-project-FAU/project/pipeLine.py
+python3 /home/runner/work/made-project-FAU/main/project/pipeLine.py
 
-db_file_employees = '/home/runner/work/made-project-FAU/data/employees_data.sqlite'
-db_file_rd_expenditure = '/home/runner/work/made-project-FAU/data/R&D_Expenditure.sqlite'
+db_file_employees = '/home/runner/work/made-project-FAU/main/data/employees_data.sqlite'
+db_file_rd_expenditure = '/home/runner/work/made-project-FAU/main/data/R&D_Expenditure.sqlite'
 
 # Check if the output file exists
 if [ -f "$db_file_employees" ]; then
