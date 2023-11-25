dc40491ab3405cea82d4061c457d68f7b3e0128d
Muhammad Naeem UR Rehman <51140786+Malik-Naeem-Awan@users.noreply.github.com>
2023-11-25 19:32:55 +0100

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-pytest ./main/project/test_pipeline.py   
+# Change directory to the project directory
+cd /home/runner/work/made-project-FAU/main/project
+
+# Execute tests using pytest
+pytest test_pipeline.py
 
 # Execute the data pipeline
-python3 ./main/project/pipeline.py
+python3 pipeline.py
 
-db_file_employees="./main/data/employees_data.sqlite"
-db_file_rd_expenditure="./main/data/R&D_Expenditure.sqlite"
+# Define the file paths for output files
+db_file_employees='../data/employees_data.sqlite'
+db_file_rd_expenditure='../data/R&D_Expenditure.sqlite'
 
-# Check if the output file exists
+# Check if the output files exist
 if [ -f "$db_file_employees" ]; then
     echo "Yes! Output file $db_file_employees is available."
 else
     echo "No! Output file $db_file_employees is not available."
 fi
 
-# Check if the output file exists
 if [ -f "$db_file_rd_expenditure" ]; then
     echo "Yes! Output file $db_file_rd_expenditure is available."
 else
